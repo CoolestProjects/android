1cf7e4aa11bf2e3a784b52d51d12dd61a42091a7
Brian Matthews <brian.matthews@synchronoss.com>
2017-05-23 18:27:31 +0100

    Initialise BeaconManagerService correctly

--- app/src/main/kotlin/com/mttnow/coolestprojects/services/BeaconManagerService.kt
+++ app/src/main/kotlin/com/mttnow/coolestprojects/services/BeaconManagerService.kt
@@ -3,6 +3,7 @@ package com.mttnow.coolestprojects.services
 import android.app.IntentService
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.estimote.sdk.Beacon
 import com.estimote.sdk.BeaconManager
 import com.estimote.sdk.Region
@@ -10,6 +11,8 @@ import com.mttnow.coolestprojects.app.CoolestProjectsApp
 import com.mttnow.coolestprojects.models.BeaconDevice
 import com.mttnow.coolestprojects.models.BeaconRegion
 import com.mttnow.coolestprojects.network.CoolestProjectsService
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 import java.util.*
 import javax.inject.Inject
 import kotlin.collections.HashMap
@@ -42,21 +45,33 @@ class BeaconManagerService : IntentService(BeaconManagerService::class.java.simp
         beaconManager.setMonitoringListener(
                 object : BeaconManager.MonitoringListener {
                     override fun onEnteredRegion(region: Region, list: List<Beacon>) {
+                        Log.d("BeaconManagerService", "Entered region $region")
                     }
 
                     override fun onExitedRegion(region: Region) {
+                        Log.d("BeaconManagerService", "Exit region $region")
                     }
                 })
 
-        coolestProjectsService.regions().subscribe {
-            beaconManager.connect(
-                    object : BeaconManager.ServiceReadyCallback {
-                        override fun onServiceReady() {
-                            startMonitoringRegions(it)
-                        }
-                    }
-            )
-        }
+        coolestProjectsService.messages()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.d("BeaconManagerService", "Messages download - $it")
+                }
+
+        coolestProjectsService.regions()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    beaconManager.connect(
+                            object : BeaconManager.ServiceReadyCallback {
+                                override fun onServiceReady() {
+                                    startMonitoringRegions(it)
+                                }
+                            }
+                    )
+                }
     }
 
     override fun onHandleIntent(intent: Intent?) {
@@ -67,14 +82,17 @@ class BeaconManagerService : IntentService(BeaconManagerService::class.java.simp
     }
 
     private fun startMonitoringRegions(regions: List<BeaconRegion>) {
+        Log.d("BeaconManagerService", "Start monitoring regions")
         regions.forEach { startMonitoringRegion(it) }
     }
 
     private fun startMonitoringRegion(region: BeaconRegion) {
-        region.beacons.forEach { startMonitoringBeacon(region.regionId, it)}
+        Log.d("BeaconManagerService", "Start monitoring region $region")
+        region.beacons.forEach { startMonitoringBeacon(region.regionId, it) }
     }
 
     private fun startMonitoringBeacon(regionId: String, beacon: BeaconDevice) {
+        Log.d("BeaconManagerService", "Start monitoring beacon $regionId,$beacon")
         beaconManager.startMonitoring(asEstimoteRegion(regionId, beacon))
         var beacons = beaconRegions[regionId]
         if (beacons == null) {
@@ -87,6 +105,7 @@ class BeaconManagerService : IntentService(BeaconManagerService::class.java.simp
     private fun stopMonitoring(regionId: String, beacon: BeaconDevice) {
         beaconManager.stopMonitoring(asEstimoteRegion(regionId, beacon))
         beaconRegions[regionId]?.remove(beacon)
+        Log.d("BeaconManagerService", "Stop monitoring beacon $regionId,$beacon")
     }
 
     private fun asEstimoteRegion(regionId: String, beacon: BeaconDevice) = Region(regionId, UUID.fromString(beacon.uuid), beacon.major, beacon.minor)
